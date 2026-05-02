017b9eb120200c122965b87672296346986d796f
Тельназ <telnazmelikova4@gmal.com>
2026-05-02 22:22:50 +0300

    Update app message to version 2

--- OneDrive/Desktop/project/index.js
+++ OneDrive/Desktop/project/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline! tm;)');
+  res.send('Hello from CI/CD Pipeline! Version 2 tm))');
 });
 
 app.listen(port, () => {
